aacb4f4f51e5ec4594822d716691c6326a84d1e2
Paulo Faria <5126193+paulofaria@users.noreply.github.com>
2021-09-04 17:08:15 -0300

    feature: vi-mode jk key binding and more

---
+++ run_once_after-configure-darwin.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Wipe all (default) app icons from the Dock
+defaults write com.apple.dock persistent-apps -array
+
+# Show only open applications in the Dock
+defaults write com.apple.dock static-only -bool true
+
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+# Auto-hide menu bar and dock
+osascript <<EOF
+tell application "System Events"
+    tell dock preferences to set autohide to true
+end tell
+EOF
+
+
+osascript <<EOF
+tell application "System Events"
+    tell dock preferences to set autohide menu bar to true
+end tell
+EOF
+
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+
+# Hide desktop icons
+defaults write com.apple.finder CreateDesktop false
+
+# Finder: disable window animations and Get Info animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Set Home as the default location for new Finder windows
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Use column view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+
+for app in "Dock" \
+    "Finder"; do
+    killall "${app}" &> /dev/null
+done
+
---
+++ run_once_after-install-packages-darwin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install latest ruby                                                 
+rbenv install 3.0.2 -s
+rbenv global 3.0.2                                                    
+rbenv rehash                                                          
+
+# Install bundler                                                     
+gem install bundler --conservative
+
+# Install latest node LTS
+source ~/.nvm/nvm.sh
+nvm install --lts
+nvm use --lts
+
