3cd56b14da469c1ab581a7fe2e7abd3ba4483c09
Paulo Faria <5126193+paulofaria@users.noreply.github.com>
2021-09-06 13:21:36 -0300

    feature: Adds nvim lua config files

---
+++ private_dot_config/nvim/lua/nvim-web-devicons-config.lua
@@ -0,0 +1,7 @@
+local devicons = require('nvim-web-devicons')
+
+devicons.setup {
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true
+}
