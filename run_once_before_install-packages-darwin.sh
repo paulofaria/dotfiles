b4e5cdb3820d1fa14e341407797142e02396012c
Paulo Faria <5126193+paulofaria@users.noreply.github.com>
2021-09-06 13:09:33 -0300

    feature: Installs neovim

--- run_once_before_install-packages-darwin.sh
+++ run_once_before_install-packages-darwin.sh
@@ -9,6 +9,7 @@ tap "cmacrae/formulae" # Spacebar
 brew "git"
 brew "tree"
 brew "fzf"
+brew "neovim"
 brew "mas"
 brew "rbenv"
 brew "node"
@@ -42,6 +43,9 @@ git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTO
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Hide desktop icons
 defaults write com.apple.finder CreateDesktop false
