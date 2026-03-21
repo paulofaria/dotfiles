59f2664850423e5aced073157b43e1e94f4545a7
Paulo Faria <5126193+paulofaria@users.noreply.github.com>
2026-03-21 02:38:39 -0300

    fix: Refreshes shell, git, bootstrap, and nvim configs

--- private_dot_config/nvim/lua/nvim-treesitter-config.lua
+++
@@ -1,9 +0,0 @@
-local treesitter  = require('nvim-treesitter.configs')
-
-treesitter.setup { 
-    ensure_installed = 'maintained',
-    highlight = { enable = true },
-    incremental_selection = { enable = true },
-    textobjects = { enable = true },
-    indent = { enable = true },
-}
--- private_dot_config/nvim/lua/nvim-web-devicons-config.lua
+++
@@ -1,7 +0,0 @@
-local devicons = require('nvim-web-devicons')
-
-devicons.setup {
-    -- globally enable default icons (default to false)
-    -- will get overriden by `get_icons` option
-    default = true
-}
