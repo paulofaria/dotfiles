eb5e0881606b4016f32f9841669630bcc190d07b
Paulo Faria <5126193+paulofaria@users.noreply.github.com>
2026-03-21 02:56:00 -0300

    fix: Loads shell secrets from macOS Keychain

---
+++ private_dot_config/zsh/secrets.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+_keychain_env() {
+  security find-generic-password -a "$1" -s "shell-env" -w 2>/dev/null
+}
+
+typeset -g GITHUB_PERSONAL_ACCESS_TOKEN
+GITHUB_PERSONAL_ACCESS_TOKEN="$(_keychain_env GITHUB_PERSONAL_ACCESS_TOKEN)"
+[[ -n "$GITHUB_PERSONAL_ACCESS_TOKEN" ]] && export GITHUB_PERSONAL_ACCESS_TOKEN
+
+typeset -g POSTGRES_URL
+POSTGRES_URL="$(_keychain_env POSTGRES_URL)"
+[[ -n "$POSTGRES_URL" ]] && export POSTGRES_URL
+
+unset -f _keychain_env
